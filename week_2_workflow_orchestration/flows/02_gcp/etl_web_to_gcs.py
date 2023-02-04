fceba8b08d69dce2bf81145633a89238e6d67030
JohnBjorge <johnedwardbjorge@gmail.com>
2023-02-04 23:59:29 +0000

    changed file path

--- week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
@@ -35,7 +35,7 @@ def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
     # path = Path(f"./week_2_workflow_orchestration/flows/02_gcp/data/{color}/{dataset_file}.parquet")
-    path = Path(f"./data/{color}/{dataset_file}.parquet")
+    path = Path(f"../data/{color}/{dataset_file}.parquet")
     print(path)
     df.to_parquet(path, compression="gzip")
     return path
