281428cd3d1dff762f66187b5f2a593d1b3cf20d
JohnBjorge <johnedwardbjorge@gmail.com>
2023-02-05 00:53:36 +0000

    working github deployment filepaths, commented out github and uncommented local options

--- week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
@@ -34,11 +34,11 @@ def clean(df: pd.DataFrame, color: str) -> pd.DataFrame:
 @task(log_prints=True)
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
-     # for local execution
-    # path = Path(f"./data/{color}/{dataset_file}.parquet")
+    #  for local execution
+    path = Path(f"./data/{color}/{dataset_file}.parquet")
 
     # for github execution (need full path for some reason)
-    path = Path(f"/home/jbjorge/my-data-engineering-zoomcamp/week_2_workflow_orchestration/flows/02_gcp/data/{color}/{dataset_file}.parquet")
+    # path = Path(f"/home/jbjorge/my-data-engineering-zoomcamp/week_2_workflow_orchestration/flows/02_gcp/data/{color}/{dataset_file}.parquet")
     
     print(path)
     df.to_parquet(path, compression="gzip")
@@ -50,10 +50,10 @@ def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
     gcs_block = GcsBucket.load("zoom-gcs")
     # for local execution
-    # gcs_block.upload_from_path(from_path=path, to_path=path)
+    gcs_block.upload_from_path(from_path=path, to_path=path)
 
     # for github execution
-    path_git = Path(f"data/green/green_tripdata_2020_11.parquet")
+    # path_git = Path(f"data/green/green_tripdata_2020_11.parquet")
     gcs_block.upload_from_path(from_path=path, to_path=path_git)
     return
 
