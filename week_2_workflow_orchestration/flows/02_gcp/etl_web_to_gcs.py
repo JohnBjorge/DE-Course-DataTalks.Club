9424bc8e55ad45ff984213e1669dc402b579f971
JohnBjorge <johnedwardbjorge@gmail.com>
2023-02-05 00:40:51 +0000

    filepath change

--- week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
@@ -38,7 +38,7 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     # path = Path(f"./data/{color}/{dataset_file}.parquet")
 
     # for github execution
-    path = Path(f"./data/{color}/{dataset_file}.parquet")
+    path = Path(f"/home/jbjorge/my-data-engineering-zoomcamp/week_2_workflow_orchestration/flows/02_gcp/data/{color}/{dataset_file}.parquet")
     # /home/jbjorge/my-data-engineering-zoomcamp/week_2_workflow_orchestration/flows/02_gcp/data/green
     print(path)
     df.to_parquet(path, compression="gzip")
@@ -53,7 +53,7 @@ def write_gcs(path: Path) -> None:
     # gcs_block.upload_from_path(from_path=path, to_path=path)
 
     # for github execution
-    path = Path(f"/home/jbjorge/my-data-engineering-zoomcamp/week_2_workflow_orchestration/flows/02_gcp/data/green")
+    path = Path(f"./data/green/green_tripdata_2020_11.parquet")
     gcs_block.upload_from_path(from_path=path, to_path=path)
     return
 
