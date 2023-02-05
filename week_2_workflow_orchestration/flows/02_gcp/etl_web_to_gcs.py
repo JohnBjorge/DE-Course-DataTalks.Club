99cb75987a707024135400349b243f561c228f2d
JohnBjorge <johnedwardbjorge@gmail.com>
2023-02-05 00:38:54 +0000

    filepath change

--- week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/flows/02_gcp/etl_web_to_gcs.py
@@ -53,7 +53,7 @@ def write_gcs(path: Path) -> None:
     # gcs_block.upload_from_path(from_path=path, to_path=path)
 
     # for github execution
-    path = Path(f"./data/green/green_tripdata_2020_11.parquet")
+    path = Path(f"/home/jbjorge/my-data-engineering-zoomcamp/week_2_workflow_orchestration/flows/02_gcp/data/green")
     gcs_block.upload_from_path(from_path=path, to_path=path)
     return
 
